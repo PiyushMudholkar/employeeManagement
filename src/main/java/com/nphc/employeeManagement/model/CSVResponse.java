76b5e042e30c334c96797eebeb764998c186db8e
PiyushMudholkar <106373537+PiyushMudholkar@users.noreply.github.com>
2022-05-27 17:07:45 +0800

    Update CSVResponse.java
    Added tostring method and removed commets

--- src/main/java/com/nphc/employeeManagement/model/CSVResponse.java
+++ src/main/java/com/nphc/employeeManagement/model/CSVResponse.java
@@ -5,7 +5,7 @@ public class CSVResponse {
 	private boolean updateRecord; // will be set when any of the record is updated 
 	private boolean skipRecord=true; // will be set when records are not inserted
 	
-	//private String message;
+
 
 	public CSVResponse() {
 	}
@@ -26,13 +26,9 @@ public class CSVResponse {
 		this.skipRecord = skipRecord;
 	}
 
-	/*
-	 * public String getMessage() { return message; }
-	 * 
-	 * public void setMessage(String message) { this.message = message; }
-	 */
 
-	
-	
-	
+	@Override
+	public String toString() {
+		return String.format("CSVResponse [updateRecord=%s, skipRecord=%s]", updateRecord, skipRecord);
+	}
 }
